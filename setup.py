eb00cbe18baff4e151eba37dde3e290d9b253cc8
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-07-28 16:56:45 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requirements = [
 ]
 setup(
     name="LaoNLP",
-    version="0.2-dev4",
+    version="0.2-dev5",
     description="Lao Natural Language Processing library",
     long_description=readme,
     long_description_content_type="text/markdown",
