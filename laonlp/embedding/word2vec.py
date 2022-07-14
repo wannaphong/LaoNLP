e8ed9422649c6dd7dfa981762c1a3f9aea6c3635
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2022-07-14 15:51:59 +0700

    Update word2vec.py

--- laonlp/embedding/word2vec.py
+++ laonlp/embedding/word2vec.py
@@ -8,6 +8,8 @@ class Word2Vec:
     def __init__(self, model: str, corpus: str="oscar") -> None:
         self.model = model
         self.corpus = corpus
+        if self.corpus not in ["oscar"]:
+            raise NotImplementedError("LaoNLP doesn't support %s corpus." % self.corpus)
         self.load_model(self.model)
     
     def load_model(self, model: str):
@@ -16,8 +18,8 @@ class Word2Vec:
         elif model=="skip-gram":
             self.model_path = hf_hub_download(repo_id="wannaphong/Lao-Word-Embedding", filename="lao_oscar_skipgram_model.bin")
         else:
-            raise NotImplementedError("We doesn't support %s model." % model)
-        self.model_wav2vec = gensim.models.keyedvectors.KeyedVectors.load_word2vec_format(self.model_path, binary=True, encoding='utf-8-sig',unicode_errors='ignore')
+            raise NotImplementedError("LaoNLP doesn't support %s model." % model)
+        self.model_wav2vec = gensim.models.keyedvectors.KeyedVectors.load_word2vec_format(self.model_path, binary=True, encoding='utf-8-sig', unicode_errors='ignore')
     
     def get_model(self):
         return self.model_wav2vec
