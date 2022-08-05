5b6910c6f1dfe41c7820789b270eeecbb1e37481
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2022-08-06 01:42:41 +0700

    Add lao_obsolete_consonants_mapping_thai

--- laonlp/__init__.py
+++ laonlp/__init__.py
@@ -10,3 +10,21 @@ CONSONANTS = "ກຂຄງຈສຊຍດຕຖທນບປຜຝພຟມຢຣ
 VOWELS_COMBINING = "ັ"+"ິີ"+"ຶືຸ"+"ູົໍ"
 VOWELS = "ະັາ"+"ຳິີ"+"ຶືຸ"+"ູົຼ"+"ຽເແ"+"ໂໃໄ"+"ໍ"
 NUMBERS = "໑໒໓໔໕໖໗໘໙໐"
+# This is Obsolete consonants.
+# You can read at https://en.wikipedia.org/wiki/Lao_script
+lao_obsolete_consonants_mapping_thai = {
+    "ຆ":"ฆ", # PALI GHA
+    "ຉ":"ฉ", # PALI CHA
+    "ຌ":"ฌ", # PALI JHA
+    "ຎ":"ญ", # PALI NYA
+    "ຏ":"ฏ", # PALI TTA
+    "ຐ":"ฐ", # PALI TTHA
+    "ຑ":"ฑ", # PALI DDA
+    "ຒ":"ฒ", # PALI DDHA
+    "ຓ":"ณ", # PALI NNA
+    "ຘ":"ธ", # PALI DHA
+    "ຠ":"ภ", # PALI BHA
+    "ຨ":"ศ", # SANSKRIT SHA
+    "ຩ":"ษ", # SANSKRIT SSA
+    "ຬ":"ฬ", # PALI LLA
+}
