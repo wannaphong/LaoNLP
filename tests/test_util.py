b70413874862e7baee2041c2145089761fbcad84
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2022-08-06 00:31:25 +0700

    Create test_util.py

---
+++ tests/test_util.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from laonlp.util import *
+
+
+class TestTagPackage(unittest.TestCase):
+    def test_lao_digit_to_arabic_digit(self):
+        self.assertEqual(
+            lao_digit_to_arabic_digit("໑໒໓໔໕໖໗໘໙໐"),
+            '1234567890'
+        )
+    def test_arabic_digit_to_lao_digit(self):
+        self.assertEqual(
+            arabic_digit_to_lao_digit('1234567890'),
+            "໑໒໓໔໕໖໗໘໙໐"
+        )
