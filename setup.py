cf5cf85168b16f4184c60e2c3dcb7e8b3d0368e2
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2023-09-24 02:03:04 +0700

    LaoNLP 1.1.2
    - Drop dependency on AnyAscii for Lao transliteration #11
    -  Preserve terminal punctuation marks during sentence tokenization #12
    -  Fix most flake8 warnings #13
    
    Thank you @BLKSerene !

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ extras = {
 
 setup(
     name="LaoNLP",
-    version="1.1.1",
+    version="1.1.2",
     description="Lao Natural Language Processing library",
     long_description=readme,
     long_description_content_type="text/markdown",
