dca6f6fa315562e9ebb40e5c6a01247e6b51ef6d
BLKSerene <blkserene@gmail.com>
2023-10-16 11:21:31 +0800

    Make return values of laonlp.translate.word_dictionary consistent with type hint

--- laonlp/translate/mopt_dict.py
+++ laonlp/translate/mopt_dict.py
@@ -21,12 +21,12 @@ def dictionary(word: str, src: str, target: str) -> list:
     if src == "lao" and target == "eng":
         _temp = mopt_dict.get_lao_eng()
         if word not in list(_temp.keys()):
-            return None
+            return []
         return _temp[word]
     elif src == "eng" and target == "lao":
         _temp = mopt_dict.get_eng_lao()
         if word not in list(_temp.keys()):
-            return None
+            return []
         return _temp[word]
     else:
-        return word
+        return [word]
